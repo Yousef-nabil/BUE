804a2b842915decb635f865c80344abc0d8c6327
Yousef nabil <yousefelhezawy75@gmail.com>
2025-03-19 17:38:51 +0200

    GOOD

---
+++ app/api/last_connect/route.ts
@@ -0,0 +1,33 @@
+
+import { createClient } from '@supabase/supabase-js';
+import { NextRequest, NextResponse } from "next/server";
+
+const x="https://urjhtgvihhpavaenhiko.supabase.co/";
+const y="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVyamh0Z3ZpaGhwYXZhZW5oaWtvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIyMDk4NTMsImV4cCI6MjA1Nzc4NTg1M30.7Ux0t9MRl6bMW8fC0A-wQaxMiPoIZ_iCcmHCtJf75I0";
+
+const supabase = createClient(
+  x!,
+  y!
+);
+
+export async function PATCH(req: NextRequest) {
+    try {
+      const body = await req.json();
+      const { last_connect_time } = body; 
+      if (last_connect_time===undefined) {
+        return NextResponse.json({ error: "Missing required fields" }, { status: 400 });
+      }
+      const { data, error } = await supabase
+        .from("values")
+        .update({last_connect_time })
+        .eq("id", 1) 
+        .select(); 
+  
+      if (error) {
+        return NextResponse.json({ error: error.message }, { status: 500 });
+      }
+      return NextResponse.json({ message: "Success", data });
+    } catch (error) {
+      return NextResponse.json({ error: "Invalid request" }, { status: 400 });
+    }
+  }
---
+++ app/api/motor_one/route.ts
@@ -0,0 +1,36 @@
+
+import { createClient } from '@supabase/supabase-js';
+import { NextRequest, NextResponse } from "next/server";
+
+const x="https://urjhtgvihhpavaenhiko.supabase.co/";
+const y="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVyamh0Z3ZpaGhwYXZhZW5oaWtvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIyMDk4NTMsImV4cCI6MjA1Nzc4NTg1M30.7Ux0t9MRl6bMW8fC0A-wQaxMiPoIZ_iCcmHCtJf75I0";
+
+const supabase = createClient(
+  x!,
+  y!
+);
+
+export async function PATCH(req: NextRequest) {
+    try {
+      const body = await req.json();
+      const { motor_one_angle ,motor_one_dircetion} = body; 
+      if (motor_one_angle === undefined) {
+        return NextResponse.json({ error: "Missing required fields" }, { status: 400 });
+      }
+      const { data, error } = await supabase
+        .from("values")
+        .update({ motor_one_angle,motor_one_dircetion })
+        .eq("id", 1) 
+        .select(); 
+  
+      if (error) {
+        return NextResponse.json({ error: error.message }, { status: 500 });
+      }
+      return NextResponse.json({ message: "Success", data });
+    } catch (error) {
+      return NextResponse.json({ error: "Invalid request" }, { status: 400 });
+    }
+  }
+
+
+
